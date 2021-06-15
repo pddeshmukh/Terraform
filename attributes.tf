735e5af17f6c134a2f8cbb1c26dbd85373b55127
Pratik Deshmukh <pratikddeshmukh99@gmail.com>
2021-06-15 23:58:20 +0530

    added .gitignore and learnt about terraform attributes

---
+++ attributes.tf
@@ -0,0 +1,16 @@
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+output "eip" {
+  value = aws_eip.lb.public_ip
+}
+
+resource "aws_s3_bucket" "mys3bucket" {
+  bucket = "my-first-s3-bucket-using-terraform"
+
+}
+
+output "mys3bucket" {
+  value = aws_s3_bucket.mys3bucket.bucket_domain_name
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "eu-west-1"
+  access_key = "AKIA3TKN5EEUYLB26EEF"
+  secret_key = "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"
+}
