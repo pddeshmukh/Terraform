ce5de9cd57793712679cf0780a399bb2955edf5b
Pratik Deshmukh <pratikddeshmukh99@gmail.com>
2024-02-20 00:26:31 +0530

    removed key id

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIA3TKN5EEUR6LRVG6Z"
-  secret_key = "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"
+  access_key = "###############"
+  secret_key = "###############"
 }
